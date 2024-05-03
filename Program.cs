0b969ddbdec121e27e8170a01573db6513d36c3a
duyd486 <duyd486@gmail.com>
2024-05-04 01:33:14 +0700

    add

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DateTime localDate = DateTime.Now;
+        System.Console.WriteLine("Datetime Now is :" + localDate);
+    }
+}
